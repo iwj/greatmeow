3ff4e39301ce134e2b247cbf7559212abed95fcc
Jian Woo <jentlewoo@gmail.com>
2018-07-17 23:05:50 +0800

    add 2 lodash function practice

--- lodash/iwj-lodash.js
+++ lodash/iwj-lodash.js
@@ -1,3 +1,24 @@
 var iwj = {
+  // “Array” Methods
+  _.chunk(array, n) {
+    // 块：将数组按照指定长度进行分块
+    var result = []
+    while (array.length > 0) {
+      result.push(array.splice(0, n))
+    }
+    return result
+  }
+
+  _.compact(array) {
+    // 紧凑（裁员）：去掉数组里不优雅的元素 false, null, 0, "", undefined, and NaN
+    var i = 0, result = []
+    while (i < array.length) {
+      if (!(array[i] === false || array[i] === null || array[i] === 0 || array[i] === "" || array[i] === undefined || array[i] !== array[i])) {
+        result.push(array[i])
+      }
+      i++
+    }
+    return result
+  }
   
 }
