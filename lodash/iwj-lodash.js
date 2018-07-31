0e34fa4eded37e410a2688bb7b680952f5a585fc
Jian Woo <jentlewoo@gmail.com>
2018-07-31 20:25:05 +0800

    lodash practice update

--- lodash/iwj-lodash.js
+++ lodash/iwj-lodash.js
@@ -10,10 +10,11 @@ var iwj = {
   },
 
   compact: function(array) {
-    // 紧凑（裁员）：去掉数组里不优雅的元素 false, null, 0, "", undefined, and NaN
+    // 紧凑（裁员）：去掉数组里不优雅的元素 false, null, 0, "", undefined, 以及 NaN
     var i = 0, result = []
     while (i < array.length) {
-      if (!(array[i] === false || array[i] === null || array[i] === 0 || array[i] === "" || array[i] === undefined || array[i] !== array[i])) {
+      if (!(array[i] === false || array[i] === null || array[i] === 0 || 
+        array[i] === "" || array[i] === undefined || array[i] !== array[i])) {
         result.push(array[i])
       }
       i++
@@ -31,7 +32,56 @@ var iwj = {
       result = result.concat(item)
     }
     return result
+
+
+    var result = []
+    return result.concat(...arguments)
+  },
+
+  difference: function(array, ...others) {
+    // 第一个参数里，没有在后面的参数里再次数出现过的元素挑出来。拗口(。・・)ノ 简单说就相当于去重。
+    var goal = []
+    var result = []
+    goal = goal.concat(...others)
+    //console.log(...goal)
+    for (var i = 0; i < array.length; i++) {
+      if (goal.indexOf(array[i]) == -1) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+
+  differenceBy: function(arrayInspect, arrayExclude, whatWay) {
+    // 在前一个函数的基础上，按照指定的函数、数值键等进行筛选去重
   },
   
+  differenceWith: function() {
+
+  },
+
+  drop: function(array, n = 1) {
+    // 丢弃：丢弃左起 n 个元素
+    return array.slice(n)
+  },
+
+  dropRight: function() {
+    // 丢弃右侧：丢弃右侧 n 个元素
+    return array.slice(0, n)
+  },
+
+  dropRightWhile: function() {},
+
+  fill: function(array, value, start = 0, end) {
+    if (end) {
+      return array.fill(value, start, end)
+    } else {
+      return array.fill(value, start, array.length)
+    }
+  },
+
+  findIndex: function() {},
+
+  findLastIndex: function() {},
 }
 
